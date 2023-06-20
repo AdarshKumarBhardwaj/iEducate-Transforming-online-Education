1d052c08ea5a1524c122cfc3d0e3c385f58189d7
AdarshKumarBhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2023-06-20 20:02:05 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger=document.querySelector('.burger');
+navbar=document.querySelector('.navbar');
+rightnav=document.querySelector('.rightnav');
+navlist=document.querySelector('.nav-list');
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('visibility-resp');
+    navlist.classList.toggle('visibility-resp');
+    navbar.classList.toggle('hnav-resp');
+    
+})
